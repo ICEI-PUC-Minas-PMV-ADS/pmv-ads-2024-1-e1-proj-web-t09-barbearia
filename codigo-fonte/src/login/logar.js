a8f78140252ccb1427325fd38480d9d0de0effc6
victorbreno92 <victor_breno.lp@hotmail.com>
2024-06-06 21:55:13 -0300

    funções js na tela de login

--- codigo-fonte/src/login/logar.js
+++ codigo-fonte/src/login/logar.js
@@ -1,20 +1,29 @@
-function logar(){
+window.alert('Seja bem vindo! Digite o seu nome de usuário e senha ')
+function color() {
+    let botao = document.querySelector('button.btn')
+    botao.style.background = 'yellow';
+}
+function sair() {
+    let btn = document.querySelector('button.btn')
+    btn.style.background = 'darkslateblue'
+}
+function logar() {
     const usuario = document.getElementById('usuario').value;
 
     const senha = document.getElementById('senha').value;
 
     let userValid;
     let barberValid;
-    
+
 
     user = JSON.parse(localStorage.getItem('user'));
-    
+
     barbearia = JSON.parse(localStorage.getItem('barbearia'));
 
-    barberValid = barbearia ? barbearia.find(b => b.usuario == usuario && b.senha == senha): undefined;
+    barberValid = barbearia ? barbearia.find(b => b.usuario == usuario && b.senha == senha) : undefined;
 
-    userValid = user ? user.find(b => b.usuario == usuario && b.senha == senha): undefined ;
-    
+    userValid = user ? user.find(b => b.usuario == usuario && b.senha == senha) : undefined;
+
 
     if (userValid && usuario == userValid.usuario && senha == userValid.senha) {
         console.log('usuario logado');
