c18d1d11f05ee8aaadc633946e04a043b01856c9
pedrorubenspuc <pedro.bastos.1526863@sga.pucminas.br>
2024-06-08 15:20:02 -0300

    Adicionado informacoes na tela de detalhes do agendamento

--- codigo-fonte/src/detalhes agendamentos/index.js
+++ codigo-fonte/src/detalhes agendamentos/index.js
@@ -1,12 +1,16 @@
 
 let boxMenu = document.getElementById('box-menu');
+const servicos = document.getElementById('servicos');
+const confirma = document.getElementById('confirma');
 const currentUrl = window.location.href;
 const id = currentUrl.split("?id=")[1];
-const agendamento = usuarioLogado.agendamentos.find(a => a.id === id);
-console.log(agendamento);
+const agendamento = userLogado.agendamentos.find(a => a.id === id);
+
+preencheConfirma();
+preencheServicos();
 
 function confirmar() {
-    window.location.href = `../confirmacao/confirmacao.HTML?id=${id}`  
+    window.location.href = `../confirmacao/confirmacao.HTML?id=${id}`
 };
 
 //verificação do header
@@ -28,4 +32,48 @@ const navbarLinks = document.querySelector('.navbar-links');
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active');
 });
+
+function preencheServicos() {
+    servicos.innerHTML = `<div class="card">
+        <div class="card-img">
+            <img src="${agendamento.barbearia.imageBanner} alt="">
+            </div>
+        <div class="card-text">
+            <h2>Barbearia: </h2>
+            <p>${agendamento.barbearia.nome}</p>
+        </div>
+        </div>
+        <div class="card">
+            <div class="card-img">
+                <img src="${agendamento.barbeiro.imagem}" alt="">
+            </div>
+            <div class="card-text">
+                <h2>Barbeiro: </h2>
+                <p>${agendamento.barbeiro.nome}</p>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-img">
+                <img src="../assets/img/serviço-dts.jpg" alt="">
+            </div>
+            <div class="card-text">
+                <h2>Serviço: </h2>
+                <p>${agendamento.servico}</p>
+            </div>
+        </div>`
+}
+
+function preencheConfirma() {
+    confirma.innerHTML = `<div class="card-confirma">
+    <div class="confirma-text">
+        <p>Data: ${agendamento.data}</p>
+        <p>Hora: ${agendamento.horario}</p>
+        <p>Valor:R$20,00</p>
+    </div>
+
+    <div class="confirma-btn">
+        <button onClick="confirmar();" class="btn">Confirmar</button>
+    </div>
+    </div>`
+}
 
